976ed283bffc85271126c6c7870be7dfa312eabf
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-10 15:59:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 def fizzbuzz(num)
   if num % 3 == 0
     "Fizz"
-  else if num % 5 == 0
+  elseif num % 5 == 0
     "Buzz"
-  else if num % 3 == 0 && num % 5 == 0
+  elseif num % 3 == 0 && num % 5 == 0
     "fizz buzz"
+  end
 end
