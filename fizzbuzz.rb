311b17afeec270aeb8df984369ccd76b2bd468b7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-10 16:11:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   val1 = num % 3 == 0
   val2 = num % 5 == 0
-  if val1 == true
+  if val1 == true && val2 != true
     "Fizz"
-  elsif val2 == true
+  elsif val2 == true && val1 != true
     "Buzz"
   elsif val1 == true && val2 == true
     "FizzBuzz"
